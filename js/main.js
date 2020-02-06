786306985cf3015d70bd1c3fad0d12f78667663b
gubriana <gubriana@gmail.com>
2020-02-06 12:39:59 -0300

    ADD: traduccion formulario usando JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var h2=document.getElementById('form-signin-heading');
+h2.innerHTML="Por favor inicia sesión";
+
+var email=document.getElementById('inputEmail');
+email.placeholder="correo electrónico";
+
+var pass=document.getElementById('inputPassword');
+pass.placeholder="Contraseña";
+
+var datos=document.getElementsByTagName('span')[0];
+datos.innerHTML="Recordar datos";
+
+var boton=document.getElementsByTagName('button')[0];
+boton.innerHTML="Iniciar Seción";
+
+
+
+
+
